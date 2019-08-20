c60d313cbc5cd16663d668f76b4c80d41114cf8b
istsh <github3104xniiapm@gmail.com>
2019-08-20 15:04:53 +0900

    Multiple sort

--- sort.go
+++ sort.go
@@ -16,12 +16,20 @@ const (
 	DESC
 )
 
+const (
+	MaxConditions int = 3
+)
+
 // Sort struct has list of targets.
 type Sort struct {
-	slice            interface{}
-	value            reflect.Value
-	sortFuncs        []func(i, j int) bool
-	sortedFieldNames []string
+	slice          interface{}
+	value          reflect.Value
+	sortConditions []*sortCondition
+}
+
+type sortCondition struct {
+	fieldName string
+	orderType OrderType
 }
 
 // Order is initialize Sort.
@@ -42,109 +50,141 @@ func Order(slice interface{}) *Sort {
 
 // Asc in ascending order in any field.
 func (s *Sort) Asc(name string) *Sort {
-	switch len(s.sortedFieldNames) {
-	case 0, 1, 2:
-		s.reserveSort(name, DESC)
-	default:
+	if s.sorted(name) {
+		fmt.Printf("%s already selected", name)
+		return s
+	}
+	if len(s.sortConditions) > MaxConditions {
 		fmt.Printf("No more can be sorted: by %s", name)
 		return s
 	}
+
+	s.sortConditions = append(s.sortConditions, &sortCondition{
+		fieldName: name,
+		orderType: ASC,
+	})
 
 	return s
 }
 
 // Desc in descending order in any field.
 func (s *Sort) Desc(name string) *Sort {
-	switch len(s.sortedFieldNames) {
-	case 0, 1, 2:
-		s.reserveSort(name, DESC)
-	default:
+	if s.sorted(name) {
+		fmt.Printf("%s already selected", name)
+		return s
+	}
+	if len(s.sortConditions) > MaxConditions {
 		fmt.Printf("No more can be sorted: by %s", name)
 		return s
 	}
 
+	s.sortConditions = append(s.sortConditions, &sortCondition{
+		fieldName: name,
+		orderType: DESC,
+	})
+
 	return s
 }
 
-func (s *Sort) reserveSort(name string, orderType OrderType) {
-	if s.sorted(name) {
-		fmt.Printf("No more can be sorted: by %s", name)
-		return
-	}
-
-	s.addSortFunc(name, orderType)
-	s.sortedFieldNames = append(s.sortedFieldNames, name)
-}
-
 func (s *Sort) sorted(name string) bool {
-	for _, sortedFieldName := range s.sortedFieldNames {
-		if sortedFieldName == name {
+	for _, sortCondition := range s.sortConditions {
+		if sortCondition.fieldName == name {
 			return true
 		}
 	}
 	return false
 }
 
-func (s *Sort) addSortFunc(name string, orderType OrderType) {
-	t := s.value.Index(0).FieldByName(name).Type()
+// Exec performs a sort
+func (s *Sort) Exec() {
+	var sortFunc func(i, j int) bool
+
+	switch len(s.sortConditions) {
+	case 1:
+		sortFunc = s.sortByOne()
+	case 2:
+		sortFunc = s.sortByTwo()
+	case 3:
+		sortFunc = s.sortByThree()
+	}
+
+	sort.Slice(s.slice, sortFunc)
+}
+
+func (s *Sort) sortByOne() func(i, j int) bool {
+	return s.sort(0)
+}
+
+func (s *Sort) sortByTwo() func(i, j int) bool {
+	return func(i, j int) bool {
+		if s.sort(0)(i, j) {
+			return true
+		}
+		return s.sort(1)(i, j)
+	}
+}
+
+func (s *Sort) sortByThree() func(i, j int) bool {
+	return func(i, j int) bool {
+		if s.sort(0)(i, j) {
+			return true
+		} else if s.sort(1)(i, j) {
+			return true
+		}
+		return s.sort(2)(i, j)
+	}
+}
+
+func (s *Sort) sort(index int) func(i, j int) bool {
+	sortCondition := s.sortConditions[index]
+	t1 := s.value.Index(0).FieldByName(sortCondition.fieldName).Type()
 
 	var sortFunc func(i, j int) bool
 
-	switch t.Kind() {
+	switch t1.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		if orderType == ASC {
+		if sortCondition.orderType == ASC {
 			sortFunc = func(i, j int) bool {
-				return s.value.Index(i).FieldByName(name).Int() < s.value.Index(j).FieldByName(name).Int()
+				return s.value.Index(i).FieldByName(sortCondition.fieldName).Int() < s.value.Index(j).FieldByName(sortCondition.fieldName).Int()
 			}
 		} else {
 			sortFunc = func(i, j int) bool {
-				return s.value.Index(i).FieldByName(name).Int() > s.value.Index(j).FieldByName(name).Int()
+				return s.value.Index(i).FieldByName(sortCondition.fieldName).Int() > s.value.Index(j).FieldByName(sortCondition.fieldName).Int()
 			}
 		}
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		if orderType == ASC {
+		if sortCondition.orderType == ASC {
 			sortFunc = func(i, j int) bool {
-				return s.value.Index(i).FieldByName(name).Uint() < s.value.Index(j).FieldByName(name).Uint()
+				return s.value.Index(i).FieldByName(sortCondition.fieldName).Uint() < s.value.Index(j).FieldByName(sortCondition.fieldName).Uint()
 			}
 		} else {
 			sortFunc = func(i, j int) bool {
-				return s.value.Index(i).FieldByName(name).Uint() > s.value.Index(j).FieldByName(name).Uint()
+				return s.value.Index(i).FieldByName(sortCondition.fieldName).Uint() > s.value.Index(j).FieldByName(sortCondition.fieldName).Uint()
 			}
 		}
 	case reflect.Float32, reflect.Float64:
-		if orderType == ASC {
+		if sortCondition.orderType == ASC {
 			sortFunc = func(i, j int) bool {
-				return s.value.Index(i).FieldByName(name).Float() < s.value.Index(j).FieldByName(name).Float()
+				return s.value.Index(i).FieldByName(sortCondition.fieldName).Float() < s.value.Index(j).FieldByName(sortCondition.fieldName).Float()
 			}
 		} else {
 			sortFunc = func(i, j int) bool {
-				return s.value.Index(i).FieldByName(name).Float() > s.value.Index(j).FieldByName(name).Float()
+				return s.value.Index(i).FieldByName(sortCondition.fieldName).Float() > s.value.Index(j).FieldByName(sortCondition.fieldName).Float()
 			}
 		}
 	case reflect.String:
-		if orderType == ASC {
+		if sortCondition.orderType == ASC {
 			sortFunc = func(i, j int) bool {
-				return s.value.Index(i).FieldByName(name).String() < s.value.Index(j).FieldByName(name).String()
+				return s.value.Index(i).FieldByName(sortCondition.fieldName).String() < s.value.Index(j).FieldByName(sortCondition.fieldName).String()
 			}
 		} else {
 			sortFunc = func(i, j int) bool {
-				return s.value.Index(i).FieldByName(name).String() > s.value.Index(j).FieldByName(name).String()
+				return s.value.Index(i).FieldByName(sortCondition.fieldName).String() > s.value.Index(j).FieldByName(sortCondition.fieldName).String()
 			}
 		}
 	default:
 		panic(fmt.Sprintf("unsupported type: %s", s.value.Index(0).Type().Kind().String()))
 	}
 
-	s.sortFuncs = append(s.sortFuncs, sortFunc)
-}
-
-// Exec performs a sort
-func (s *Sort) Exec() {
-
-	// TODO(istsh): make sort functions
-	sortFunc := func(i, j int) bool {
-		return false
-	}
-
-	sort.Slice(s.slice, sortFunc)
+	return sortFunc
 }
